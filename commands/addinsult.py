21a9aff20fa1a945e028f10f7d3fb6aa8f906af7
nukeop <alsw@protonmail.com>
2017-12-03 22:39:38 +0100

    addinsult stub

--- commands/addinsult.py
+++ commands/addinsult.py
@@ -1,1 +1,18 @@
+from database import db
 
+def create_command(bot):
+
+    def init_db():
+        if db.get('insults') is None:
+            db.set('insults', [])
+        
+
+    init_db()
+    print(db.get('insults'))
+    
+    @bot.command(pass_context=True, brief="Adds a new insult to the database.")
+    async def addinsult(ctx, *args):
+        insult = ' '.join(args)
+        
+
+    return addinsult
