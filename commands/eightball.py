783a8afd98cfca65ca539cd3f50296d697c450c8
nukeop <alsw@protonmail.com>
2017-12-04 16:01:35 +0100

    Add 8ball command

---
+++ commands/eightball.py
@@ -0,0 +1,33 @@
+import random
+
+answers = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes, definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "All signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+]
+
+def create_command(bot):
+
+    @bot.command(name='8ball', pass_context=True, brief="You got a question, you ask the 8-ball.")
+    async def eightball(ctx):
+        """
+        Answers a yes/no question.
+        """
+        await bot.say(random.choice(answers))
