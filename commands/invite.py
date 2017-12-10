3821a43465136ea4ecd24304ee4f6e3f27ed18ed
nukeop <alsw@protonmail.com>
2017-12-10 21:22:22 +0100

    Invite command

---
+++ commands/invite.py
@@ -0,0 +1,12 @@
+INVITE_URL = "https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=0"
+
+def create_command(bot):
+
+    @bot.command(pass_context=True, brief="PMs you an invite link so you can add this bot to your channel")
+    async def invite(ctx):
+        await bot.say("Use the link below to add me to your server.")
+        await bot.say(INVITE_URL.format(bot.user.id))
+        await bot.say("You can only add me to servers you own or administrate.")
+
+    return invite
+    
