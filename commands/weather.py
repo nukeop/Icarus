5848dce84e529bc8bbe122bbe35239a6855dec78
nekonee <haniasa96@gmail.com>
2017-12-11 01:43:18 +0100

    Workig weather command

--- commands/weather.py
+++ commands/weather.py
@@ -1,24 +1,24 @@
 import json
 import requests
 
-
-apikey = config["OPENWEATHERMMAP_APIKEY"]
-API= requests.get("https://github.com/nukeop/Icarus/blob/master/commands/crypto.py"
-                  +location
-                  +"&units=metric&APPID="
-                  +apikey).text
-parsed = json.loads(API)
+from config import config
 
 
 
 def get_weather(location):
+    apikey = config["openweathermap_apikey"]
+    r= requests.get("http://api.openweathermap.org/data/2.5/find?q="+location+"&units=metric&appid="+apikey).text
+    parsed = json.loads(r)
+
     weather_conditions=[]
-    for w in parsed['weather']:
+    print(parsed)
+    print(parsed['message'])
+    for w in parsed['list'][0]['weather']:
         weather_conditions.append(w['main'])
 
 
-    msg= "Weather in {}: \nTemperature:{} C \nConditions:{}\nWind speed: {} m/s\nCloudiness: {}%"\
-         .format(parsed["name"].encoded("utf-8"),parsed["main"]["temp"], ", ".join(weather_conditions), parsed["wind"]["speed"], parsed["cloud"]["all"])
+    msg= "Weather in {}: \nTemperature: {} C \nConditions: {}\nWind speed: {} m/s\nCloudiness: {}%"\
+         .format(parsed['list'][0]["name"], parsed['list'][0]["main"]["temp"], ", ".join(weather_conditions), parsed['list'][0]["wind"]["speed"], parsed['list'][0]["clouds"]["all"])
     return msg
     
 
@@ -30,7 +30,7 @@ def generate_help_string():
 
 def create_command(bot):
     @bot.command(pass_context=True, brief="Shows weather", help=generate_help_string())
-    async def weather(ctx, *, location):
+    async def weather (ctx, *, location):
         await bot.say(get_weather(location))
 
 
