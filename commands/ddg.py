5c81fc1af9309ef1f9a0a43be7a087284f16a639
nukeop <alsw@protonmail.com>
2017-12-14 11:31:57 +0100

    Improved ddg command that returns the first link if instant answer is not found

--- commands/ddg.py
+++ commands/ddg.py
@@ -1,8 +1,21 @@
 import json
 import requests
 import urllib.parse
+from lxml import html
 
 DDG_API_URL = "https://api.duckduckgo.com/?q={}&format=json"
+DDG_SEARCH_URL = "https://duckduckgo.com/html/"
+
+def ddg_search(term):
+    params = {
+        'q': term,
+        's': '0'
+    }
+    text = requests.post(DDG_SEARCH_URL, data=params).text
+    document = html.fromstring(text)
+    
+    result = document.cssselect('#links .links_main a')[0].get('href')
+    return result
 
 def create_command(bot):
 
@@ -27,7 +40,9 @@ def create_command(bot):
             relatedstr = ""
 
             if len(related) < 1:
-                await bot.say("No information about term {}.".format(term))
+                await bot.say(ddg_search(term))
+                
+                #await bot.say("No information about term {}.".format(term))
                 return
 
             for i, entry in enumerate(related):
