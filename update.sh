c527671e4e50e033184efc59090318ebee3f9ddd
nukeop <12746779+nukeop@users.noreply.github.com>
2017-12-14 14:45:01 +0100

    Update update.sh
    Install dependencies on update

--- update.sh
+++ update.sh
@@ -2,5 +2,7 @@ ps aux | grep icarus.py | cut -c10-15 | xargs kill
 cp config.py config.bak
 git reset --hard
 git pull origin master
+source venv/bin/activate
+pip install -r requirements.txt
 cp config.bak config.py
 python icarus.py --updated
