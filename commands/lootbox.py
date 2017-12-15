3d2a9644b213fb63542d779a3d17b52cf59480b7
nukeop <alsw@protonmail.com>
2017-12-15 18:37:51 +0100

    Show lootbox contents through embed

--- commands/lootbox.py
+++ commands/lootbox.py
@@ -1,6 +1,8 @@
 import random
 import time
 
+from discord import Colour, Embed
+
 lootbox_contents = open("data/lootbox.txt").readlines()
 
 def create_command(bot):
@@ -9,4 +11,12 @@ def create_command(bot):
     async def lootbox(ctx):
         await bot.say("{} is opening a loot box... :gift:".format(ctx.message.author.mention))
         time.sleep(3)
-        await bot.say(random.choice(lootbox_contents))
+
+        embed = Embed()
+        embed.type = "rich"
+        embed.color = Colour.gold()
+        embed.add_field(
+            name="The contents are yours...",
+            value=random.choice(lootbox_contents)
+        )
+        await bot.say(None, embed=embed)
