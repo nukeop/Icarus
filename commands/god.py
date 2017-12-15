0a902f4920cd2857e1ab46008181fd7ad3911ec8
nukeop <alsw@protonmail.com>
2017-12-15 23:04:01 +0100

    Add a way to communicate with space alien

---
+++ commands/god.py
@@ -0,0 +1,22 @@
+import random
+
+from discord import Colour, Embed
+
+words = open("data/GodWordsHappy.txt").readlines()
+
+def get_words(words, num):
+    return " ".join([random.choice(words).strip() for _ in range(num)])
+
+def create_command(bot):
+
+    @bot.command(pass_context=True, brief="Allows you to communicate with God")
+    async def god(ctx, num=10):
+        embed = Embed(
+            description=get_words(words, num)
+        )
+        embed.type = "rich"
+        embed.color = Colour.blue()
+        
+        await bot.say("God says...", embed=embed)
+
+    return god
