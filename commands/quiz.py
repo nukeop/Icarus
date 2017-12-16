df7822e27d5faed6406b51735d3b973b63e14dc5
nukeop <alsw@protonmail.com>
2017-12-16 12:41:08 +0100

    Format quiz question in an embed

--- commands/quiz.py
+++ commands/quiz.py
@@ -1,7 +1,30 @@
+import html
 import json
+import random
 import requests
 
+from discord import Colour, Embed
+
 QUIZ_API = "https://opentdb.com/api.php?amount=1"
+
+def difficulty_to_stars(difficulty):
+    if difficulty == 'easy':
+        return ':star:'
+    elif difficulty == 'medium':
+        return ':star:'*2
+    elif difficulty == 'hard':
+        return ':star:'*3
+    else:
+        return 'Unknown'
+
+def format_answers(correct, incorrect):
+    incorrect.append(correct)
+    answers = incorrect
+    random.shuffle(answers)
+
+    answers = '\n'.join(['{}. {}'.format(x+1, y) for (x, y) in enumerate(answers)])
+    
+    return answers
 
 def create_command(bot):
 
@@ -11,7 +34,26 @@ def create_command(bot):
         question = json.loads(question)
         question = question['results'][0]
 
-        message = []
-        message.append("Category: {}")
+        embed = Embed()
+        embed.type="rich"
+        embed.color = Colour.green()
+        embed.add_field(
+            name="Category",
+            value=question['category']
+        )
+        embed.add_field(
+            name="Difficulty",
+            value=difficulty_to_stars(question['difficulty'])
+        )
+        embed.add_field(
+            name="Question",
+            value=html.unescape(question['question'])
+        )
+        embed.add_field(
+            name="Answers",
+            value=format_answers(question['correct_answer'], question['incorrect_answers'])
+        )
+
+        await bot.say(None, embed=embed)
 
     return quiz
