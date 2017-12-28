62fc7ac4fc5e882fe7d0c8c7add48d7be2adc66a
nukeop <alsw@protonmail.com>
2017-12-28 22:57:58 +0100

    Improve comic command

--- commands/comic.py
+++ commands/comic.py
@@ -3,24 +3,29 @@ import requests
 
 from discord import Colour, Embed
 
-API_URL_XKCD = "http://xkcd.com/info.0.json"
+API_URL_XKCD_LATEST = "http://xkcd.com/info.0.json"
+API_URL_XKCD = "http://xkcd.com/{}/info.0.json"
 
-def get_comic():
-    requested = requests.get(API_URL_XKCD).text
-    parsed = json.loads(requested)
+def get_xkcd_latest():
+    return json.loads(requests.get(API_URL_XKCD_LATEST).text)
 
-    picture = parsed["img"]
-    return picture
+def get_xkcd(i):
+    return json.loads(requests.get(API_URL_XKCD.format(i)).text)
+
+def xkcd_embed(data):
+    embed = Embed()
+    embed.type = "rich"
+    embed.title = 'XKCD - {}: {}'.format(data['num'], data['safe_title'])
+    embed.description = data['alt']
+    embed.set_image(
+        url=data['img']
+    )
+
+    return embed
 
 def create_command(bot):
     @bot.command(pass_context= True, brief = "xkcd comic")
     async def comic(ctx):
-        picture = get_comic()
-
-        embed = Embed()
-        embed.type = "rich"
+        embed = xkcd_embed(get_xkcd_latest())
         embed.color = Colour.dark_orange()
-        embed.set_image(
-            url = picture
-        )
         await bot.say(None, embed=embed)
