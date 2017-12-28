9c62a01a5a3cbff96c4f4afb343c7dd501787bae
nekonee <haniasa96@gmail.com>
2017-12-28 20:40:22 +0100

    😂👌 got rid of random newlines

--- commands/comic.py
+++ commands/comic.py
@@ -3,22 +3,18 @@ import requests
 
 from discord import Colour, Embed
 
-
 API_URL_XKCD = "http://xkcd.com/info.0.json"
-
 
 def get_comic():
     requested = requests.get(API_URL_XKCD).text
     parsed = json.loads(requested)
 
     picture = parsed["img"]
-
     return picture
 
 def create_command(bot):
     @bot.command(pass_context= True, brief = "xkcd comic")
     async def comic(ctx):
-
         picture = get_comic()
 
         embed = Embed()
