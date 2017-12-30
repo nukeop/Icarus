645c381cc9763d92a6b1db0031638dfd6744c130
nekonee <haniasa96@gmail.com>
2017-12-30 19:04:43 +0100

    Random neko image

---
+++ commands/neko.py
@@ -0,0 +1,31 @@
+import json
+import requests
+
+from discord import Colour, Embed
+
+
+NEKO_API = "http://nekos.life/api/neko"
+
+def get_neko():
+    r = requests.get(NEKO_API).text
+    parsed = json.loads(r)
+
+    neko = parsed["neko"]
+
+    return neko
+
+
+def create_command(bot):
+
+    @bot.command(pass_context = True, brief = "Random neko image for you")
+    async def neko(ctx):
+        neko = get_neko()
+        
+        embed = Embed()
+        embed.type = "rich"
+        embed.color = Colour.magenta()
+
+        embed.set_image(
+            url = neko
+        )
+        await bot.say(None, embed=embed)
