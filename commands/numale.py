d40ac7bddcfbfc4ca12aaf0236ccb7e3bc29cf08
nekonee <haniasa96@gmail.com>
2017-12-30 22:56:16 +0100

    Random numale face

--- commands/numale.py
+++ commands/numale.py
@@ -1,6 +1,17 @@
+import random
+
+numales = [
+    "https://i.imgur.com/Bo2bSDN.jpg",
+    "https://i.imgur.com/TYhCjdf.jpg",
+    "https://i.imgur.com/QVwovqY.jpg",
+    "https://i.imgur.com/G373kWA.jpg",
+    "https://i.imgur.com/7Dhu1nR.jpg",
+]
+
+
 def create_command(bot):
 
     @bot.command(pass_context=True, brief="Posts that face (you know which "
                  "one)")
     async def numale(ctx):
-        await bot.say('https://i.imgur.com/Bo2bSDN.jpg')
+        await bot.say(random.choice(numales))
