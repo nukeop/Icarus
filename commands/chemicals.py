a383d2ee2b56c10c63eb900c916e760baba13e41
nukeop <12746779+nukeop@users.noreply.github.com>
2018-04-11 15:36:02 +0200

    Update chemicals.py

--- commands/chemicals.py
+++ commands/chemicals.py
@@ -2,12 +2,12 @@ import random
 import string
 
 def get_chemical():
-    return random.choice(string.ascii_lowercase) + random.choice(string.ascii_uppercase)
+    return random.choice(string.ascii_lowercase) + '**' + random.choice(string.ascii_uppercase) + '**'
 
 def create_command(bot):
 
     @bot.command(pass_context=True, brief="Generates random chemicals")
     async def chemicals(ctx):
-        await bot.say('\n'.join([get_chemical() for _ in range(5)]))
+        await bot.say(' '.join([get_chemical() for _ in range(5)]))
 
     return chemicals
